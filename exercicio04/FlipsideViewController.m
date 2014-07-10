dfcb7561890b283876c4d6305ab8b0a9bd59da1b
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2014-07-10 17:21:52 -0300

    Initial Commit

---
+++ exercicio04/FlipsideViewController.m
@@ -0,0 +1,36 @@
+//
+//  FlipsideViewController.m
+//  exercicio04
+//
+//  Created by Jackson F. de A. Mafra on 10/07/2014.
+//  Copyright (c) 2014 Jackson F. de A. Mafra. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface FlipsideViewController ()
+
+@end
+
+@implementation FlipsideViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Actions
+
+- (IBAction)done:(id)sender
+{
+    [self.delegate flipsideViewControllerDidFinish:self];
+}
+
+@end
---
+++ exercicio04/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  exercicio04
+//
+//  Created by Jackson F. de A. Mafra on 10/07/2014.
+//  Copyright (c) 2014 Jackson F. de A. Mafra. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
